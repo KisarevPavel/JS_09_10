27aa6985f3c980b346122b872bd7a3d641a70cb8
Kisarev Pavel <ppcppcppc12@mail.ru>
2016-08-18 22:03:56 +0300

    first commit

---
+++ addons/scripts.js
@@ -0,0 +1,55 @@
+
+    $(function() {
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+			
+			
+			
+			$(".js-states").select2();
+			
+			
+			
+			
+			$( '.dropdown' ).hover(
+				function(){
+					$(this).children('.sub-menu').slideDown(200);
+				},
+				function(){
+					$(this).children('.sub-menu').slideUp(200);
+				}
+			);
+			
+			
+			
+			
+    });
